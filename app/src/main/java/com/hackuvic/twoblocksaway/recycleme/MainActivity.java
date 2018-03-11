5265a50c1b37a7e14abbac2d20d7442a3c1fe887
Ryan937 <lsj937@gmail.com>
2018-03-11 00:29:59 -0800

    Finished the starting page

--- app/src/main/java/com/hackuvic/twoblocksaway/recycleme/MainActivity.java
+++ app/src/main/java/com/hackuvic/twoblocksaway/recycleme/MainActivity.java
@@ -1,5 +1,8 @@
 package com.hackuvic.twoblocksaway.recycleme;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
